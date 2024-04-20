68f85c5d0e598777648ec2ec84b20f41f232fbc2
bnguyensn <b.nguyen.sn@gmail.com>
2024-04-20 22:55:13 +0700

    Add initial DateTimeFormat page

---
+++ src/pages/DateTimeFormatPage.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { useDateTimeFormatOptions } from '../hooks/useDateTimeFormatOptions';
+
+export function DateTimeFormatPage() {
+  const {
+    state: { locale, ...formatOptions },
+    dispatch,
+  } = useDateTimeFormatOptions();
+
+  const [input, setInput] = useState('2024-01-01T00:00');
+
+  return (
+    <main>
+      <h1>
+        <pre>Intl.DateFormat</pre>
+      </h1>
+      <div className="formatter-container">
+        <input
+          type="datetime-local"
+          id="intl-date-time-format"
+          name="intl-date-time-format"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+    </main>
+  );
+}
